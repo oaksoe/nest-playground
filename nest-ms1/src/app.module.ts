8625fe1cc3a347f289189d2075492098a3b3bf35
Oak Soe <oaksoekyaw@gmail.com>
2019-06-03 14:01:10 +0800

    added middleware: logger

--- nest-ms1/src/app.module.ts
+++ nest-ms1/src/app.module.ts
@@ -1,8 +1,10 @@
-import { Module } from '@nestjs/common';
+import { Module, MiddlewareConsumer, RequestMethod  } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { CatsModule } from './cats/cats.module';
+import { CatsController } from './cats/cats.controller';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { LoggerMiddleware } from './middlewares/logger.middleware';
 
 @Module({
   imports: [
@@ -11,4 +13,17 @@ import { UsersModule } from './users/users.module';
     AuthModule, 
     UsersModule],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(LoggerMiddleware)
+      //.forRoutes('cats');
+      // .forRoutes({ path: 'cats', method: RequestMethod.GET });
+      // .forRoutes({ path: 'ab*cd', method: RequestMethod.ALL });
+      .exclude(
+        { path: 'cats', method: RequestMethod.GET },
+        { path: 'cats', method: RequestMethod.POST }
+      )
+      .forRoutes(CatsController);
+  }
+}
